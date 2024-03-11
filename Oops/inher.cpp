ba053592cb978c376df0dbe03ca57821e045d0b7
nd2812 <nisarg2005prajapati@gmail.com>
2024-03-11 20:35:27 +0530

    fifteen commit

---
+++ Oops/inher.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class human{
+    // protected:
+        int height;
+    public:
+        int weight;
+    // private:
+        int age;
+
+    public:
+    int getAge(){
+        return this->age;
+    }
+    void setWeight(int w){
+        this->weight=w;
+    }
+
+};
+
+class male:public human{
+    public:
+    string colour;
+
+    void sleep(){
+        cout<<"male sleeping"<<endl;
+    }
+    int getHeight(){
+        return this->height;
+    }
+};
+
+
+int main() {
+    male m1;
+
+    cout<<m1.getHeight()<<endl;
+    // cout<<m1.height<<endl;
+
+
+
+
+
+
+    // male object1;
+    // cout<<object1.age<<endl;
+    // cout<<object1.weight<<endl;
+    // cout<<object1.height<<endl;
+    // cout<<object1.colour<<endl;
+    // object1.setWeight(84);
+    // cout<<object1.weight<<endl;
+
+    // object1.sleep();
+    return 0;
+}

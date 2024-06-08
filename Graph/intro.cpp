6fed5d9fd8d9d1ef9c862a6673a6b6c99c65515d
nd2812 <nisarg2005prajapati@gmail.com>
2024-06-08 18:37:52 +0530

    30 commit

--- Graph/intro.cpp
+++ Graph/intro.cpp
@@ -28,6 +28,69 @@ class graph{
         }
 };
 
+
+// BFS(Breadth First Search) in graph
+vector<int> bfsTraversal(int n, vector<vector<int>> &adj){
+    vector<int> ans;
+    unordered_map<int,bool> visited;
+    queue<int> q;
+
+    q.push(0);
+    while(!q.empty()){
+        int front=q.front();
+        q.pop();
+        if(!visited[front]){
+
+            visited[front]=true;
+            
+            ans.push_back(front);
+
+            for(int i=0;i<adj[front].size();i++){
+                q.push(adj[front][i]);
+            }
+        }
+    }
+    return ans;
+}
+
+// DFS(Depth First Search) in graph
+void dfs(int node,unordered_map<int,bool> &visited,unordered_map<int,list<int>> &adj,vector<int> &comp){
+    comp.push_back(node);
+    visited[node]=true;
+
+    // hr connected node k liye recursive call
+    for(auto i:adj[node]){
+        if(!visited[i]){
+            dfs(i,visited,adj,comp);
+        }
+    }
+}
+
+vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+{
+    // prepare adjList
+    unordered_map<int,list<int>> adj;
+    for(int i=0;i<edges.size();i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    unordered_map<int,bool> visited;
+    vector<vector<int>> ans;
+
+    for(int i=0;i<V;i++){
+        if(!visited[i]){
+            vector<int> comp;
+            dfs(i,visited,adj,comp);
+            ans.push_back(comp);
+        }
+    }
+    return ans;
+}
+
 int main() {
     int n;
     cout<<"Enter the number of nodes "<<endl;
